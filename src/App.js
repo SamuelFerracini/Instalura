82a62764f1ec579859894149472c74938b902493
Samuel <samuel.ferracini@hotmail.com>
2019-09-24 10:21:52 -0300

    Atualizacoes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './css/componentes/Header';
+import Timeline from './css/componentes/Timeline';
+
+class App extends Component {
+  render() {    
+    return (
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline login={this.props.params.login}/>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
